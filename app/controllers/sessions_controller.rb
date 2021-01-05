2f506f25bfc5f106774e60f9cc754ffd57b7510e
sergiocardenasg <sergiocardenas@utexas.edu>
2021-01-04 19:50:31 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    if !@user.nil?
-      if @user.authenticate(params[:user][:password])
-          session[:user_id] = @user.id
-          redirect_to '/'
-      end 
+    @user = User.find_by(name: params[:name])
+    if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to '/'
     else  
       redirect_to '/login'
     end 
-  end
 end
