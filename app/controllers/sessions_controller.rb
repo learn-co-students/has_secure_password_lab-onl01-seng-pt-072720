884a89e7f8f10e3c337102c18dc96204544221b5
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-06 14:54:39 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,11 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(name: params[:name])
-        if @user && @user.authenticate(params[:password])
+        @user = User.find_by(name: user_params[:name]) # regular `params` aren't automatically permitted, hence calling on the private `user_params` method
+        if @user && @user.authenticate(user_params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            # return head(:forbidden)
             redirect_to login_path
         end
     end
@@ -16,6 +15,6 @@ class SessionsController < ApplicationController
     private 
 
     def user_params
-        params.require(:user).permit(:username, :password, :password_confirmation)
+        params.require(:user).permit(:name, :password, :password_confirmation)
     end
 end
